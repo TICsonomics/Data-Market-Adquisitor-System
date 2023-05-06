25e2bbdda49c37340dce6b18e0108aa945071a24
Ignacio Ireta <ignacio.ireta@outlook.com>
2023-05-05 18:05:45 -0600

    Changed app output, as well as database input. Corrected several bugs. Completed documentation

--- app/app.py
+++ app/app.py
@@ -17,6 +17,7 @@ def fetch_data_from_api(url):
 
 
 def get_ohcl(coin_id, vs_currency, days):
+
     """Returns a Pandas DataFrame with timestamp, open, high, low, close data
 
     Parameters
@@ -34,6 +35,7 @@ def get_ohcl(coin_id, vs_currency, days):
     30 days: 4 hours
     max: 4 days
     """
+
     historical_url = f'https://api.coingecko.com/api/v3/coins/{coin_id}/ohlc?vs_currency={vs_currency}&days={days}'
     historical_data = fetch_data_from_api(historical_url)
 
@@ -45,6 +47,7 @@ def get_ohcl(coin_id, vs_currency, days):
 
 
 def get_volume(coin_id, vs_currency, days):
+
     """Returns a Pandas DataFrame with timestamp, volume data
 
     Parameters
@@ -61,6 +64,7 @@ def get_volume(coin_id, vs_currency, days):
     1 day from current time = 5 minute interval data
     30 days from current time = hourly data
     max = daily data (00:00 UTC)"""
+
     volume_url = f"https://api.coingecko.com/api/v3/coins/{coin_id}/market_chart?vs_currency={vs_currency}&days={days}"
     volume_data = fetch_data_from_api(volume_url)
 
@@ -71,18 +75,23 @@ def get_volume(coin_id, vs_currency, days):
 
 
 def convert_timestamp(df):
-    """Returns the given dataframe with the unix_timestamp converted into a more readable format  YYYY-MM-DD HH:MM:SS
+
+    """Converts the Unix Timestamp to a more readable format, namely YYYY-MM-DD HH:MM:SS
 
     Parameters
 
     df : pandas.core.frame.DataFrame"""
-    df['date_price'] = df['unix_timestamp'].apply(lambda a: (a / 1000.0) - ((a / 1000.0) % (30 * 60))).apply(
-        lambda b: datetime.fromtimestamp(b))
-    df = df.drop(columns=['unix_timestamp'])
+
+    df['date_price'] = df['unix_timestamp'].apply(
+        lambda a: (a / 1000.0) - ((a / 1000.0) % (30 * 60))  # Converts milliseconds to seconds and subtracts the
+        # difference between the time the record was taken, and the closest half hour
+    ).apply(lambda b: datetime.fromtimestamp(b))  # Converts the adjusted timestamp using Python's datetime library
+    df = df.drop(columns=['unix_timestamp'])  # Drops the unix_timestamp column to avoid duplicates
     return df
 
 
 def pull_coin_data(coin_id, vs_currency, days):
+
     """Returns a single DataFrame containing the OHLC prices, volume, and timestamps
     for the given coin, vs_currency and timeframe (days)
 
@@ -94,6 +103,7 @@ def pull_coin_data(coin_id, vs_currency, days):
         The target currency of market data(usd, eur, jpy, etc.)
     days : str
         Data up to number of days ago(1 / 30 / max)"""
+
     ohcl = get_ohcl(coin_id, vs_currency, days)
     ohcl = convert_timestamp(ohcl)
     time.sleep(10)
@@ -101,26 +111,19 @@ def pull_coin_data(coin_id, vs_currency, days):
     volume = convert_timestamp(volume)
     time.sleep(10)
     merged = ohcl.join(volume.set_index('date_price'), on='date_price')
+    # Reorder the columns
+    merged = merged[['date_price', 'open_price', 'high_price', 'low_price', 'close_price', 'volume']]
+    # Drop duplicated values in the date_price column
+    merged.drop_duplicates(subset='date_price', keep='first', inplace=True)
+    # Since the API returns an empty volume record for the last one, we drop it
+    merged.drop(index=merged.index[-1], axis=0, inplace=True)
     return merged
 
 
-timeframes = {'half_hour': '1', 'four_hours': '30', 'four_days': 'max'}
-ticker_vs_currency = ['ripple', 'usd']
-data = {key: pull_coin_data(*ticker_vs_currency, values) for key, values in timeframes.items()}
-# Ikke bekymre deg for noe over denne linjen. FOKUS
-
-db_name = 'data_acquisition'
-db_user = 'acquisition'
-db_pass = '0PZ9TVXV'
-db_host = 'db'
-db_port = '5432'
-
-# Connect to the database
-db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
-db = create_engine(db_string)
-
-
 def get_latest_timestamp(db, table_name):
+
+    """Returns the latest timestamp stored in the table's database"""
+
     metadata = MetaData()
     try:
         table = Table(table_name, metadata, schema="public", autoload_with=db)
@@ -132,6 +135,19 @@ def get_latest_timestamp(db, table_name):
 
 
 def store_data_to_db(df, db, table_name):
+
+    """Stores a DataFrame to a database
+
+    Parameters:
+
+    df : pandas.core.frame.DataFrame
+        DataFrame to be stored
+    db :
+        SQLAlchemy's engine object
+    days : str
+        Data up to number of days ago(1 / 30 / max)
+    """
+
     latest_timestamp = get_latest_timestamp(db, table_name)
 
     if latest_timestamp is not None:
@@ -142,10 +158,21 @@ def store_data_to_db(df, db, table_name):
     df.to_sql(table_name, db, if_exists="append", index=False)
 
 
-# table_names = {'half_hour': 'half_hour_table', 'four_hours': 'four_hours_table', 'four_days': 'four_days_table'}
+timeframes = {'half_hour': '1', 'four_hours': '30', 'four_days': 'max'}
+ticker_vs_currency = ['ripple', 'usd']
+data = {key: pull_coin_data(*ticker_vs_currency, values) for key, values in timeframes.items()}
+
+db_name = 'data_acquisition'
+db_user = 'acquisition'
+db_pass = '0PZ9TVXV'
+db_host = 'db'
+db_port = '5432'
+
+# Connect to the database
+db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
+db = create_engine(db_string)
 
 for key, values in data.items():
     store_data_to_db(values, db, key)  # table_names[key]
     
 print('Done')
-
